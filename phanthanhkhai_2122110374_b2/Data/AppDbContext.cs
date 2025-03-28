dabb603f0dafb2c066e6441fb2dda8441f225402
PhanThanhKhai <khaiphan.02082004@gmail.com>
2025-03-28 16:17:19 +0700

    first commit

---
+++ phanthanhkhai_2122110374_b2/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using phanthanhkhai_2122110374_b2.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+
+namespace phanthanhkhai_2122110374_b2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ phanthanhkhai_2122110374_b2/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace phanthanhkhai_2122110374_b2.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Price { get; set; }
+
+    }
+}
