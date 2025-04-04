4163f3930133d166ad68b43ea14efb4d1342a511
Phan Thanh Khai <khaiphan.02082004@gmail.com>
2025-04-04 16:15:50 +0700

    Commit ban đầu

---
+++ phanthanhkhai_2122110374_b2/Model/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace phanthanhkhai_2122110374_b2.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Cat_Id { get; set; }
+        public string Cat_Name { get; set; }
+        public string Image { get; set; }
+    }
+}
